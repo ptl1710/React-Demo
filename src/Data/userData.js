93e50d70ac809f249966ad7c1271fff3712c7917
longpt <longpt@dtp-education.com>
2023-03-14 16:24:37 +0700

    update data

--- src/Data/userData.js
+++ src/Data/userData.js
@@ -20,13 +20,34 @@ let useAccount = [
     },
     {
         idUser:2,
-        username: 'longpt',
-        password: '171000',
-        name: 'Phạm Thành Long',
+        username: 'thenq',
+        password: '041296',
+        name: 'Ngô Quang Thế',
+        imageUrl:'https://media.istockphoto.com/id/1338134336/photo/headshot-portrait-african-30s-man-smile-look-at-camera.jpg?b=1&s=170667a&w=0&k=20&c=j-oMdWCMLx5rIx-_W33o3q3aW9CiAWEvv9XrJQ3fTMU=',
         phone: '0961108123',
-        address: 'Số nhà 10, ngõ 99, đường Tây Sơn, phường Trung Liệt, quận Đống Đa, Hà Nội.',
+        address: 'Số nhà 10, ngõ 99, đường Tây Sơn, phường Trung Liệt, quận HoocMôn, Thành phố Hồ Chí Minh.',
         role: 'user'
-    }
+    },
+    {
+        idUser:3,
+        username: 'anv',
+        password: '041296',
+        name: 'Nguyễn Văn A',
+        imageUrl:'https://media.istockphoto.com/id/1338134336/photo/headshot-portrait-african-30s-man-smile-look-at-camera.jpg?b=1&s=170667a&w=0&k=20&c=j-oMdWCMLx5rIx-_W33o3q3aW9CiAWEvv9XrJQ3fTMU=',
+        phone: '0961108123',
+        address: 'Số nhà 123, ngõ 111, đường Tây Sơn 2, phường Trung Liệt 3, quận HoocMôn, Thành phố Hồ Chí Minh.',
+        role: 'user'
+    },
+    {
+        idUser:4,
+        username: 'bnt',
+        password: '041296',
+        name: 'Nguyễn Thị B',
+        imageUrl:'https://media.istockphoto.com/id/1338134336/photo/headshot-portrait-african-30s-man-smile-look-at-camera.jpg?b=1&s=170667a&w=0&k=20&c=j-oMdWCMLx5rIx-_W33o3q3aW9CiAWEvv9XrJQ3fTMU=',
+        phone: '0961108123',
+        address: 'Số nhà 13232, ngõ 9123129, đường Tây Sơn 3, phường Trung Liệt 4, quận HoocMôn, Thành phố Hồ Chí Minh.',
+        role: 'user'
+    },
 ]
 
 export default useAccount
